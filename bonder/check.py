02e04bfe96bf8486ef3e8222e4a4cad4312a2830
Graham Krizek <gkrizek@nodesource.com>
2018-09-28 14:17:19 -0500

    Write CLI

--- bonder/check.py
+++ bonder/check.py
@@ -1,5 +1,69 @@
+import click
+import json
+import subprocess
+from sys import exit
 
 
-def check_bonder(verbose):
-    print('checked')
+def get_unbonded_atom(address):
+    process = subprocess.Popen("gaiacli account %s" % (address), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if process.returncode is 0:
+        return {
+            "success": True,
+            "code": process.returncode,
+            "output": process.stdout.read()
+        }
+    else:
+        return {
+            "success": False,
+            "code": process.returncode,
+            "output": process.stdout.read()
+        }
+    return
+
+
+def bond_steak(address_validator, name, steak):
+    process = subprocess.Popen("gaiacli stake delegate --amount=%s --address-validator=%s --name=%s" % (steak, address_validator, name), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if process.returncode is 0:
+        return {
+            "success": True,
+            "code": process.returncode,
+            "output": process.stdout.read()
+        }
+    else:
+        return {
+            "success": False,
+            "code": process.returncode,
+            "output": process.stdout.read()
+        }
+    return
+
+
+def check_bonder(address, address_validator, name, verbose):
+    if verbose:
+        click.echo("Checking for address %s" % (address))
+    account = get_unbonded_atom(address)
+    if not account['success']:
+        click.secho("Error getting account information:", fg="red", bold=True)
+        print(account)
+        exit(1)
+    account = json.loads(account['output'])
+    coin_list = account['value']['coins']
+    for c in coin_list:
+        if c['denom'] == 'steak':
+            steak = c['amount']
+
+    if verbose:
+        click.echo("Found %s steak" %(steak))
+
+    if steak > 0:
+        bond = bond_steak(name, address, steak)
+        if not bond['success']:
+            click.secho("Error bonding steak:", fg="red", bold=True)
+            print(bond)
+            exit(1)
+        click.echo("Successfully bonded steak:")
+        print(bond)
+
+
+
     return
--- bonder/index.py
+++ bonder/index.py
@@ -2,29 +2,44 @@
 import click
 from time import sleep
 from sys import exit
-from .check import check_atom
+from .check import check_bonder
 from .__init__ import __version__
-
-# Constants
-CHECK_INTERVAL = 10
-
 
 @click.group()
 def bonder():
     """
     \b
-    BONDER TEXT
+     _ )   _ \   \ |  _ \  __|  _ \ 
+     _ \  (   | .  |  |  | _|     / 
+    ___/ \___/ _|\_| ___/ ___| _|_\ 
+                                
 
     Bonder - Atom Auto-Bonding for a Cosmos Validator
     """
 
 
 @bonder.command('start', short_help="start bonder")
+@click.option('--address', help="address to check for unbonded steak", metavar='<ADDRESS>')
+@click.option('--address_validator', help="validator address to bond to", metavar='<ADDRESS>')
+@click.option('--name', help="name of address to delegate from", metavar='<ADDRESS>')
+@click.option('--interval', default=10, help="Number of seconds to wait in between checks")
 @click.option('--verbose', default=False, is_flag=True, help="add additional logging")
-def start(verbose):
+def start(address, address_validator, interval, name, verbose):
+    if not address or not address_validator or not name:
+        click.secho("Error: You must provide an address, address_validator, and name. Please use the appropriate flags", fg="red", bold=True)
+        exit(1)
+    first_run = True
     while True:
-        check_bonder(verbose)
-        sleep(CHECK_INTERVAL)
+        if first_run and verbose:
+            click.echo("Starting Bonder...")
+            click.echo("Checking every %s seconds" % (interval))
+            click.echo("Address: " % (address))
+            click.echo("Validator Address" % (address_validator))
+            click.echo("Name" % (name))
+        check_bonder(address, address_validator, name, verbose)
+        first_run = False
+        sleep(interval)
+
 
 
 @bonder.command('version', short_help="check agent version")
